43dcfe964f6bdf6217bc3dd6069012ae804e33e1
roby2358 <roby2358@yahoo.com>
2025-12-31 14:21:42 -0800

    faihelpers 0.1.0

--- src/main.py
+++ src/main.py
@@ -1,18 +1,15 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from fastapi import Request
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/")
+async def read_root():
+    return FileResponse("static/index.html")
 
 
 @app.get("/health")
